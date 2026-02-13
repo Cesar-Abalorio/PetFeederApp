e564c53781b4ba4c25db532207df56d448227775
Cesar-Abalorio <abalorio.cesar@gmail.com>
2026-02-13 19:22:55 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Login from "./Login";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <App /> 
+      <Login />
     </HashRouter>
   </React.StrictMode>
 );
