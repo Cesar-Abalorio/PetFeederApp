98ffbb4ce11f57e75c3e4592d7c77b43cc1819a7
Cesar-Abalorio <abalorio.cesar@gmail.com>
2026-02-13 19:05:59 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./Login";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename= {import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
+    <HashRouter>
+      <App />
     </HashRouter>
   </React.StrictMode>
 );
