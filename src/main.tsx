b58302acc1aaff02944bd5a76d3cbcbcafca15f4
Cesar-Abalorio <abalorio.cesar@gmail.com>
2026-02-13 18:55:34 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './Login.css'
-import Login from './Login'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Login />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/PetFeederApp/">
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
